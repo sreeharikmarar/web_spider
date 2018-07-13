c54fedacfc6888b5813652874d334772d12eb7a3
sreeharikmarar <sreeharikmarar@gmail.com>
2018-07-13 11:49:08 +0530

    keeping the file open for future writes

--- lib/web_spider/sitemap.rb
+++ lib/web_spider/sitemap.rb
@@ -2,10 +2,10 @@ require "builder"
 
 module WebSpider
   class Sitemap
-    attr_accessor :sitemap, :images
+    attr_reader :sitemap, :images
 
     def initialize(file_path)
-      @sitemap = File.new(file_path,"w")
+      @sitemap = File.open(file_path, File::WRONLY | File::APPEND | File::CREAT)
       @images = Array.new
     end
 
@@ -16,9 +16,7 @@ module WebSpider
     def update_sitemap(url_string)
       builder = Builder::XmlMarkup.new(:indent=>2)
       entry = builder.url { |url| url.loc(url_string); update_images(url); }
-      File.open(@sitemap,"ab") do |f|
-        f.write(entry)
-      end
+      @sitemap.write(entry)
       @images.clear
     end
 
